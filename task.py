28e2930e4de570e1653fdf5a48cb77376637ef50
kartikey1232 <90847424+kartikey1232@users.noreply.github.com>
2024-12-09 21:10:04 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,26 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+password=[]
+for num in range(1,nr_letters+1):
+    password.append(letters[random.randint(0,len(letters)-1)])
+for num in range(1,nr_symbols+1):
+    password.append(symbols[random.randint(0,len(symbols)-1)])
+for num in range(1,nr_numbers+1):
+    password.append(numbers[random.randint(0,len(numbers)-1)])
+print((password))
+final=[]
+for num in range(1,len(password)+1):
+    final=random.sample(password,len(password))
+print(final)
+for items in final:
+    print(items,end=" ")
+
+
+
